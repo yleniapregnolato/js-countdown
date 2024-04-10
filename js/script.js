ee2ecf5af944a2db8d544e36a13244812b9ff9dc
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-10 14:35:13 +0200

    feat = I define new variable and print

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// definisco una varabile per i secondi che parte da 10
+
+let seconds = 10; // number
+
+// prelevo l'elemento HTML da assegnare alla variabile seconds e strampo i secondi nell'elemento html
+
+document.getElementById("seconds").innerText = seconds;
+
